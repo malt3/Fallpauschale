b49bc9726a4293b3709a761b5f6514c1b96ecf83
R0qs3T <torbenrogge@gmail.com>
2014-09-13 21:49:15 +0200

    added new radiobutton to limit-entries at list

--- web/js/controller/elementController.js
+++ web/js/controller/elementController.js
@@ -1,4 +1,13 @@
-angular.module("app").controller("elementController", function ($scope){
+angular.module("app").controller("elementController", function ($scope, $http){
+		$http.get("Fallpauschalen.json").success(function (data){
+		$scope.entries = data;
+		$scope.maxentries= data.length;
+		$scope.entries.forEach(function(entry){
+			entry.relation= Math.round(entry.relation*3156.82)
+		
+		});
+	})
 	$scope.listOptions={limit:500};
+	
 
 })
--- web/js/controller/listController.js
+++ web/js/controller/listController.js
@@ -1,10 +1,5 @@
 angular.module("app").controller("listController", function ($scope, $http) {
-	$http.get("Fallpauschalen.json").success(function (data){
-		$scope.entries = data;
-		$scope.entries.forEach(function(entry){
-			entry.relation= Math.round(entry.relation*3156.82)
-		});
-	})
+
 
 	$scope.orderVar = 'name';
 	$scope.reverse = false;
