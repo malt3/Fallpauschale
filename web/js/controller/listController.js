dce0877c322a8c0f6f1a4b7817cd52c236f0ed73
Malte Poll <malte.poll@web.de>
2014-09-13 20:58:42 +0200

    Pfeile

--- web/js/controller/listController.js
+++ web/js/controller/listController.js
@@ -9,6 +9,45 @@ angular.module("app").controller("listController", function ($scope, $http) {
 	$scope.orderVar = 'name';
 	$scope.reverse = false;
 
+	$scope.showName = function (orientation){
+		if ($scope.orderVar == "name") {
+			if (orientation == "up") {
+				if ($scope.reverse)
+					{return true}
+				else
+					{return false};
+
+			}
+			if (orientation == "down") {
+				if ($scope.reverse) {
+					{return false}
+				}else{
+					{return true}
+				}
+			};
+		}else{return false}
+	}
+
+	$scope.showRelation = function (orientation){
+		if ($scope.orderVar == "relation") {
+			if (orientation == "up") {
+				if ($scope.reverse)
+					{return true}
+				else
+					{return false};
+
+			}
+			if (orientation == "down") {
+				if ($scope.reverse) {
+					{return false}
+				}else{
+					{return true}
+				}
+			};
+		}else{return false}
+	}
+
+
 	$scope.changeOrder = function(order){
 		if ($scope.orderVar == order) {
 			$scope.reverse = !$scope.reverse;
