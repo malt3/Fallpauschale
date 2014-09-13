893f1b6edfac07f1f7f9a475730fd65123c6f4d6
Malte Poll <malte.poll@web.de>
2014-09-13 15:59:43 +0200

    LINTED

--- translate_raw/converter.py
+++ translate_raw/converter.py
@@ -7,7 +7,7 @@ with open('../raw/Fallpauschalenkatalog 2014_20140120.csv', 'rb') as input_file:
 		output_file.write("[")
 		for row in reader:
 			identifier = row[0]
-			name = row[2]
+			name = row[2].replace("\r", "")
 			relation = row[3]
 			if len(identifier) == 4:
 				output_file.write('{"name":"%s","relation":"%s"},'%(name,relation.replace(',','.')))
