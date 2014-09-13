c5a278157a0ceef47073b8fee8cf171dde4f9528
w33bion <speckeldy@gmx.de>
2014-09-13 22:25:27 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	web/index.html

--- web/js/controller/elementController.js
+++ web/js/controller/elementController.js
@@ -1,4 +1,13 @@
-angular.module("app").controller("elementController", function ($scope){
-	$scope.listOptions={limit:500};
+angular.module("app").controller("elementController", function ($scope, $http){
+		$http.get("Fallpauschalen.json").success(function (data){
+		$scope.entries = data;
+		$scope.maxentries= data.length;
+		$scope.listOptions={limit: $scope.maxentries};
+		$scope.entries.forEach(function(entry){
+			entry.relation= Math.round(entry.relation*3156.82)
+		
+		});
+	})
+	
 
 })
--- web/js/controller/listController.js
+++ web/js/controller/listController.js
@@ -1,51 +1,8 @@
 angular.module("app").controller("listController", function ($scope, $http) {
-	$http.get("Fallpauschalen.json").success(function (data){
-		$scope.entries = data;
-		$scope.entries.forEach(function(entry){
-			entry.relation= Math.round(entry.relation*3156.82)
-		});
-	})
+
 
 	$scope.orderVar = 'name';
 	$scope.reverse = false;
-
-	$scope.showName = function (orientation){
-		if ($scope.orderVar == "name") {
-			if (orientation == "up") {
-				if ($scope.reverse)
-					{return true}
-				else
-					{return false};
-
-			}
-			if (orientation == "down") {
-				if ($scope.reverse) {
-					{return false}
-				}else{
-					{return true}
-				}
-			};
-		}else{return false}
-	}
-
-	$scope.showRelation = function (orientation){
-		if ($scope.orderVar == "relation") {
-			if (orientation == "up") {
-				if ($scope.reverse)
-					{return true}
-				else
-					{return false};
-
-			}
-			if (orientation == "down") {
-				if ($scope.reverse) {
-					{return false}
-				}else{
-					{return true}
-				}
-			};
-		}else{return false}
-	}
 
 
 	$scope.changeOrder = function(order){
