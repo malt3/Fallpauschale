499e53250ca1caf04d6666a19c4b8869c582396a
Malte Poll <malte.poll@web.de>
2014-09-13 20:19:31 +0200

    Ordering works

--- web/js/controller/listController.js
+++ web/js/controller/listController.js
@@ -2,7 +2,19 @@ angular.module("app").controller("listController", function ($scope, $http) {
 	$http.get("Fallpauschalen.json").success(function (data){
 		$scope.entries = data;
 		$scope.entries.forEach(function(entry){
-entry.relation= Math.round(entry.relation*3156.82)
-});
+			entry.relation= Math.round(entry.relation*3156.82)
+		});
 	})
+
+	$scope.orderVar = 'name';
+	$scope.reverse = false;
+
+	$scope.changeOrder = function(order){
+		if ($scope.orderVar == order) {
+			$scope.reverse = !$scope.reverse;
+		}else{
+			$scope.orderVar = order;
+			$scope.reverse = false;
+		}
+	}
 });
