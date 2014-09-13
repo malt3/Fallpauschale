63c219551f734054893551cd6aab695c0ce5d06c
Malte Poll <malte.poll@web.de>
2014-09-13 13:46:29 +0200

    deleted empty list

--- translate_raw/translate.py
+++ translate_raw/translate.py
@@ -3,7 +3,6 @@ import csv
 
 with open('../raw/Fallpauschalenkatalog 2014_20140120.csv', 'rb') as f:
 	reader = csv.reader(f,delimiter=';')
-	result = []
 	for row in reader:
 		identifier = row[0]
 		name = row[2]
